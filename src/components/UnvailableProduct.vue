bc707aaf9d8f58d5fbdf9b204b91a80f00c2056c
lusi <129147732+lista259@users.noreply.github.com>
2024-05-12 23:30:39 +0700

    Create UnvailableProduct.vue

---
+++ src/components/UnvailableProduct.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="productCategoryClass">
+    <!-- Product information goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    productCategoryClass() {
+      return this.product.category === 'Unavailable Product' ? 'product-unavailable' : 'product-available';
+    }
+  }
+}
+</script>
